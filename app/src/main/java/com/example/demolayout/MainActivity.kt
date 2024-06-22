8a40cfef6044d828251618829ef744b5919f846b
TMThanh7217 <onthidaihocvatly12@gmail.com>
2024-06-23 03:20:14 +0700

    Add more layout demo

--- app/src/main/java/com/example/demolayout/MainActivity.kt
+++ app/src/main/java/com/example/demolayout/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.demolayout
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.GridView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -14,26 +17,58 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // setContentView(R.layout.frame_layout_demo)
+
+        // Remove comment and run to view the layout
+        // Except for GridView and ListView, the rest doesn't need additional code
+        // Example remove comment on setContentView(R.layout.linear_layout_demo
+        // comment the rest of unnecessary code to load the Linear layout
+
         //setContentView(R.layout.frame_layout_demo)
+        //setContentView(R.layout.linear_layout_demo)
         //setContentView(R.layout.absolute_layout_demo)
-        setContentView(R.layout.grid_view_layout)
+        //setContentView(R.layout.relative_layout_demo)
+        //setContentView(R.layout.table_layout_demo)
+        setContentView(R.layout.constraint_layout_demo)
+
+        // grid view layout demo code
+        /*setContentView(R.layout.grid_view_layout)
         gridViewDemo = findViewById(R.id.gridViewDemo)
         val cardItemArrayList: ArrayList<CardItem?> = ArrayList<CardItem?>()
 
-        cardItemArrayList.add(CardItem("DSA", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("JAVA", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("C++", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("ad", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("s212", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("cc", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("zz", R.drawable.ic_launcher_background))
-        cardItemArrayList.add(CardItem("C--", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Chair", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Bird", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Cat", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Dog", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Television", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Phone", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Car", R.drawable.ic_launcher_background))
+        cardItemArrayList.add(CardItem("Fish", R.drawable.ic_launcher_background))
 
         val adapter = GridViewLayoutAdapter(this, cardItemArrayList)
-        gridViewDemo.adapter = adapter
-        //setContentView(R.layout.linear_layout_demo)
-        //setContentView(R.layout.relative_layout_demo)
+        gridViewDemo.adapter = adapter*/
+        // end grid view demo
+
+        // list view layout demo code
+        /*setContentView(R.layout.list_view_layout)
+        val arrayAdapter: ArrayAdapter<*>
+        val myOsValue = arrayOf(
+            "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+            "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+            "Android", "iPhone", "WindowsMobile"
+        )
+
+        // access the listView from xml file
+        var listViewDemo = findViewById<ListView>(R.id.listViewDemo)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myOsValue)
+        listViewDemo.adapter = arrayAdapter*/
+        // end list view layout demo
+
+        val text = "Hello toast!"
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(this, text, duration).show()
 
     }
 }
